f74217d300b64d2b028be4cbcc36dad81d6ed6e6
Volodymyr Tkhoryk <volodymyrx.tkhoryk@intel.com>
2019-11-26 15:00:30 +0200

    Add project files.

---
+++ MathMeth_2/Program.cs
@@ -0,0 +1,74 @@
+﻿using QueueingSystem;
+
+namespace MathMeth_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First task
+            
+            var queue1 = new MonoChannel(0.1, 9, 10);
+            queue1.PrintResults();
+
+            /*
+            var queue2 = new MonoChannel(0.1, 5);
+            queue2.PrintResults();
+
+            var queue3 = new MonoChannel(0.1, 9);
+            queue3.PrintResults();
+
+            var queue4 = new MonoChannel(0.2, 1);
+            queue4.PrintResults();
+
+            var queue5 = new MonoChannel(0.5, 1);
+            queue5.PrintResults();
+
+            var queue6 = new MonoChannel(0.9, 1);
+            queue6.PrintResults();
+
+            var queue7 = new MonoChannel(2, 0.1);
+            queue7.PrintResults();
+
+            var queue8 = new MonoChannel(5, 0.1);
+            queue8.PrintResults();
+
+            var queue9 = new MonoChannel(9, 0.1);
+            queue9.PrintResults();
+            */
+            #endregion
+
+            /*
+            #region Second task
+            var multi = new MultiChannel(4, 2, 10);
+            multi.PrintResults();
+
+            var multi2 = new MultiChannel(1, 5, 10);
+            multi2.PrintResults();
+
+            var multi3 = new MultiChannel(0.3, 9, 10);
+            multi3.PrintResults();
+
+            var multi4 = new MultiChannel(4, 1, 5);
+            multi4.PrintResults();
+
+            var multi5 = new MultiChannel(3, 1, 5);
+            multi5.PrintResults();
+
+            var multi6 = new MultiChannel(1, 1, 5);
+            multi6.PrintResults();
+
+            var multi7 = new MultiChannel(190, 0.1, 20);
+            multi7.PrintResults();
+
+            var multi8 = new MultiChannel(100, 0.1, 20);
+            multi8.PrintResults();
+
+            var multi9 = new MultiChannel(20, 0.1, 20);
+            multi9.PrintResults();
+            #endregion
+            */
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ QueueingSystem/MonoChannel.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace QueueingSystem
+{
+    public class MonoChannel
+    {
+        /// <summary>
+        /// Average intensity of incoming calls flow
+        /// </summary>
+        double lambda;
+
+        /// <summary>
+        /// Average time between successful call income
+        /// </summary>
+        double iat;
+
+        /// <summary>
+        /// Average servicing time
+        /// </summary>
+        double st;
+
+        /// <summary>
+        /// Expected service channel load
+        /// </summary>
+        double rho;
+
+        double r;
+
+        int channels = 1;
+        int queueMaxSize;
+
+        double avgCallsAmount;
+        double avgCallsAmountInQueue;
+        double avgTimeInSystem;
+        double avgTimeInQueue;
+        double probMaxQueue;
+        double probSystemFree;
+        double probSystemBusy;
+
+        public MonoChannel(double _lambda, double _st, int _queueSize, int _channels = 1)
+        {
+            lambda = _lambda;
+            iat = 1 / lambda;
+            st = _st;
+            queueMaxSize = _queueSize;
+            rho = lambda * st / _channels;
+            r = lambda * st;
+
+            Calculation();
+        }
+
+        public void Calculation()
+        {
+            for (int s = 0; s < channels; s++)
+                probSystemFree += Math.Pow(channels * rho, s) / Factorial(s);
+            probSystemFree += 
+                (Math.Pow(channels * rho, channels) / Factorial(channels)) * (1 - Math.Pow(rho, queueMaxSize + 1)) / (1 - rho);
+            probSystemFree = 1 / probSystemFree;
+            for (int i = 0; i < channels; i++)
+                avgCallsAmount += Math.Pow(r, i) / Factorial(i);
+            avgCallsAmount *= r * probSystemFree;
+            avgCallsAmount += (rho * ProbabilityOfCallsInSystem(channels) / Math.Pow(1 - rho, 2))
+                * (1 + channels*(1 - rho) - (1 + Math.Pow(rho, queueMaxSize)*(1 - rho)*(channels + queueMaxSize)));
+
+            avgCallsAmountInQueue = (rho * ProbabilityOfCallsInSystem(channels) / Math.Pow(1 - rho, 2))
+                * (1 - Math.Pow(rho, queueMaxSize) * (1 + queueMaxSize * (1 - rho)));
+
+            double a = (rho * ProbabilityOfCallsInSystem(channels) / Math.Pow(1 - rho, 2));
+            double b = (1 - Math.Pow(rho, queueMaxSize) * (1 + queueMaxSize * (1 - rho)));
+
+            avgCallsAmountInQueue = a * b;
+            probMaxQueue = ProbabilityOfCallsInSystem(queueMaxSize);
+
+            for (int k = 0; k < queueMaxSize; k++)
+                avgTimeInQueue += k + 1;
+            avgTimeInQueue *= ProbabilityOfCallsInSystem(channels + queueMaxSize) * st / channels;
+
+            avgTimeInSystem = avgTimeInQueue + st;
+
+        }
+
+        private double ProbabilityOfCallsInSystem(int calls)
+        {
+            return calls < channels
+                ? Math.Pow(channels * rho, calls) / Factorial(calls) * probSystemFree
+                : Math.Pow(channels * rho, calls) / (Factorial(channels) * Math.Pow(channels, calls - channels)) * probSystemFree;
+        }
+
+        private long Factorial(int x) => x == 0 ? 1 : x * Factorial(--x);
+
+        public void PrintResults()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"\t\t\tLAMBDA = {lambda}; st = {st}; K = {queueMaxSize}");
+            Console.WriteLine(
+                $"rho \t= {rho}\n" +
+                $"L \t= {avgCallsAmount:N3}\n" +
+                $"Lq \t= {avgCallsAmountInQueue:N3}\n" +
+                $"W \t= {avgTimeInSystem:N3}\n" +
+                $"Wq \t= {avgTimeInQueue:N7}\n" +
+                $"p0 \t= {probSystemFree:N5}\n" +
+                $"p(k) \t= {probMaxQueue:N7}\n");
+        }
+    }
+}
